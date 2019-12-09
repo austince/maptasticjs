82ce16aa8b685aaf1a389cd8322785b25e6cd360
austin ce <austin.cawley@gmail.com>
2019-12-09 15:43:26 -0500

    build: use rollup, amd => umd w/ esm support

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import { readFileSync } from 'fs';
+import babel from 'rollup-plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
+import commonjs from 'rollup-plugin-commonjs';
+
+const pkg = JSON.parse(readFileSync('./package.json').toString());
+
+export default {
+  input: 'src/maptastic.js',
+  external: ['numeric'],
+  output: {
+    name: pkg.name,
+    banner: `/*! ${pkg.name} ${(new Date().toISOString())} */`,
+    format: 'umd',
+    globals: { numeric: 'numeric' }
+    ,
+  },
+  plugins: [
+    commonjs(),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    uglify({
+      compress: {
+        pure_getters: true,
+        unsafe_comps: true,
+      }
+    }),
+  ],
+};
